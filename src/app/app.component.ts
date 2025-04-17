ff9a982413201923d343472bfee02a9e7311e8a2
Ali <alideraralialiali@gmail.com>
2025-04-17 15:29:04 +0300

    Admin Dashboard
    Manage users (approve/reject artisan registrations).
    View all job postings and transactions.
     Generate reports on platform activity.
     Remove inappropriate job listings or user profiles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-
-import { TestComponent } from './test/test.component';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ManageUsersComponent } from "./Component/Admin/manage-users/manage-users.component";
+import { JobPostingsComponent } from "./Component/Admin/job-postings/job-postings.component";
+import { ReportsSectionComponent } from "./Component/Admin/reports-section/reports-section.component";
+import { AdminDashboardComponent } from "./Component/Admin/admin-dashboard/admin-dashboard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [TestComponent],
+  standalone: true,
+  imports: [RouterModule, CommonModule, ManageUsersComponent, JobPostingsComponent, ReportsSectionComponent, AdminDashboardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'THP';
+users: any;
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+constructor(private router: Router) {}
+
+  closeModal() {
+    this.router.navigate([{ outlets: { popup: null }}]);
+  }
+  get isModalOpen(): boolean {
+    return location.href.includes('(popup:');
+  }
 }
