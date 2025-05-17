820d4277552190fb63dfb27165eff939bc445536
Ali <alideraralialiali@gmail.com>
2025-05-17 18:59:14 +0300

    backend with front end

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -6,11 +6,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class JobService {
-  private formPostApiUrl = 'http://127.0.0.1:8085/api/form-posts';
+  private adminApiUrl = 'http://127.0.0.1:8085/api/admin';
 
   constructor(private http: HttpClient) {}
 
+  // Get all form posts
   getFormPosts(): Observable<any> {
-    return this.http.get<any>(this.formPostApiUrl);
+    return this.http.get<any>(`${this.adminApiUrl}/form-posts`);
+  }
+
+  // Delete specific form post by ID
+  deleteFormPost(id: number): Observable<any> {
+    return this.http.delete(`${this.adminApiUrl}/form-posts/${id}`);
+  }
+
+  // Get total count of form posts
+  getFormPostCount(): Observable<any> {
+    return this.http.get<any>(`${this.adminApiUrl}/form-posts-count`);
+  }
+
+  // Get count of Artisan users
+  getArtisanCount(): Observable<any> {
+    return this.http.get<any>(`${this.adminApiUrl}/artisan-count`);
   }
 }
