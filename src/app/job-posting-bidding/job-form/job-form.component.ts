82473f37020a32913c5937ef189c1d22e8d5fc5f
waleed khalid <151838143+waleedarman@users.noreply.github.com>
2025-06-09 04:48:07 +0300

    Update job-form.component.ts

--- src/app/job-posting-bidding/job-form/job-form.component.ts
+++ src/app/job-posting-bidding/job-form/job-form.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Router, RouterLink } from '@angular/router';
 import Swal from 'sweetalert2'; 
+import { PostService } from '../../services/post.service';
 
 interface AttachedFile {
   file: File;
@@ -14,9 +14,10 @@ interface AttachedFile {
 @Component({
   selector: 'app-job-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './job-form.component.html',
-  styleUrl: './job-form.component.css'
+  styleUrl: './job-form.component.css',
+  providers: [PostService]
 })
 export class JobFormComponent implements OnInit {
   title: string = 'Post a New Job';
@@ -35,7 +36,7 @@ export class JobFormComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private http: HttpClient,
+    private postService: PostService, 
     private router: Router
   ) {}
 
@@ -80,7 +81,7 @@ export class JobFormComponent implements OnInit {
 
     const formData = new FormData();
 
-    formData.append('user_id', '1');
+    
     formData.append('title', this.jobForm.value.jobTitle);
     formData.append('description', this.jobForm.value.jobDescription);
     formData.append('minimum_budget', this.jobForm.value.minBudget);
@@ -93,16 +94,16 @@ export class JobFormComponent implements OnInit {
       formData.append('attachment', this.attachedFiles[0].file, this.attachedFiles[0].name);
     }
 
-    this.http.post('http://localhost:8000/api/posts', formData).subscribe({
+    this.postService.createPost(formData).subscribe({
       next: (res) => {
-        Swal.fire('Success ', 'Post saved successfully', 'success');
+        Swal.fire('Success', 'Post saved successfully', 'success');
         this.jobForm.reset();
         this.attachedFiles = [];
         this.router.navigate(['/JobPostingBiddingComponent/post']);
       },
       error: (err) => {
         console.error(err);
-        Swal.fire('Error ', 'An error occurred while storing.', 'error');
+        Swal.fire('Error', 'An error occurred while storing.', 'error');
       }
     });
   }
